e7591081e288299b70116919d849a474f796947c
hifihedgehog <16614343+hifihedgehog@users.noreply.github.com>
2026-03-26 04:18:32 -0400

    Fix vJoy macro parity gaps, deduplicate PF_SetupApi PS template
    ExecuteSingleActionRaw (vJoy macro path) was missing features present
    in ExecuteSingleAction (gamepad path):
    - SystemVolume: add InvertAxis + ShowVolumeOsd passthrough
    - AppVolume: add InvertAxis
    - MouseMove: add InvertAxis
    - MouseScroll: add InvertAxis
    
    Extract duplicate PF_SetupApi inline C# template from DriverInstaller
    and VJoyVirtualController into shared GetPsSetupApiSnippet() method in
    SetupApiInterop. Prevents future refactor regressions like the
    SetupApiInterop.SP_DEVINFO_DATA bug from 010b0ca.

--- PadForge.App/Common/Input/VJoyVirtualController.cs
+++ PadForge.App/Common/Input/VJoyVirtualController.cs
@@ -2073,30 +2073,7 @@ try {{
     $dst = ""$env:SystemRoot\System32\drivers\vjoy.sys""
     if (-not (Test-Path $dst) -and (Test-Path $src)) {{ Copy-Item $src $dst -Force }}
 
-    Add-Type -TypeDefinition @'
-using System;
-using System.Runtime.InteropServices;
-public static class PF_SetupApi {{
-    public const int DIF_REGISTERDEVICE = 0x19;
-    public const int SPDRP_HARDWAREID = 0x01;
-    public const int DICD_GENERATE_ID = 0x01;
-    [StructLayout(LayoutKind.Sequential)]
-    public struct SP_DEVINFO_DATA {{ public int cbSize; public Guid ClassGuid; public int DevInst; public IntPtr Reserved; }}
-    [DllImport(""setupapi.dll"", SetLastError = true)]
-    public static extern IntPtr SetupDiCreateDeviceInfoList(ref Guid ClassGuid, IntPtr hwndParent);
-    [DllImport(""setupapi.dll"", SetLastError = true, CharSet = CharSet.Unicode)]
-    public static extern bool SetupDiCreateDeviceInfoW(IntPtr DeviceInfoSet, string DeviceName, ref Guid ClassGuid, string DeviceDescription, IntPtr hwndParent, int CreationFlags, ref SP_DEVINFO_DATA DeviceInfoData);
-    [DllImport(""setupapi.dll"", SetLastError = true, CharSet = CharSet.Unicode)]
-    public static extern bool SetupDiSetDeviceRegistryPropertyW(IntPtr DeviceInfoSet, ref SP_DEVINFO_DATA DeviceInfoData, int Property, byte[] PropertyBuffer, int PropertyBufferSize);
-    [DllImport(""setupapi.dll"", SetLastError = true)]
-    public static extern bool SetupDiCallClassInstaller(int InstallFunction, IntPtr DeviceInfoSet, ref SP_DEVINFO_DATA DeviceInfoData);
-    [DllImport(""setupapi.dll"", SetLastError = true)]
-    public static extern bool SetupDiDestroyDeviceInfoList(IntPtr DeviceInfoSet);
-    [DllImport(""newdev.dll"", SetLastError = true, CharSet = CharSet.Unicode)]
-    [return: MarshalAs(UnmanagedType.Bool)]
-    public static extern bool UpdateDriverForPlugAndPlayDevicesW(IntPtr hwndParent, string HardwareId, string FullInfPath, int InstallFlags, out bool bRebootRequired);
-}}
-'@
+" + SetupApiInterop.GetPsSetupApiSnippet() + $@"
     $hidGuid = [Guid]::new('{{745a17a0-74d3-11d0-b6fe-00a0c90f57da}}')
     $hwid = 'root\VID_1234&PID_BEAD&REV_0222'
     $infPath = '{vjoyDir.Replace("'", "''")}\vjoy.inf'

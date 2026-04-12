35ad6bb595c0df4273c13a9f9649815f8b4b03ee
hifihedgehog <16614343+hifihedgehog@users.noreply.github.com>
2026-04-12 02:32:33 -0400

    Bump version to 2.4.0

--- PadForge.Engine/Properties/AssemblyInfo.cs
+++ PadForge.Engine/Properties/AssemblyInfo.cs
@@ -14,5 +14,5 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("A1B2C3D4-E5F6-7890-ABCD-EF1234567890")]
 
-[assembly: AssemblyVersion("2.3.0.0")]
-[assembly: AssemblyFileVersion("2.3.0.0")]
+[assembly: AssemblyVersion("2.4.0.0")]
+[assembly: AssemblyFileVersion("2.4.0.0")]

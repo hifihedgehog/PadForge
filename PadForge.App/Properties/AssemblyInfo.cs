c1ad050c473a3c45b8cb67e5df1f21282f4f1a3e
hifihedgehog <16614343+hifihedgehog@users.noreply.github.com>
2026-04-29 17:25:37 -0400

    Bump to v3.0.5

--- PadForge.App/Properties/AssemblyInfo.cs
+++ PadForge.App/Properties/AssemblyInfo.cs
@@ -20,5 +20,5 @@ using System.Windows;
 
 [assembly: Guid("F0E1D2C3-B4A5-6789-0123-456789ABCDEF")]
 
-[assembly: AssemblyVersion("3.0.4.0")]
-[assembly: AssemblyFileVersion("3.0.4.0")]
+[assembly: AssemblyVersion("3.0.5.0")]
+[assembly: AssemblyFileVersion("3.0.5.0")]
